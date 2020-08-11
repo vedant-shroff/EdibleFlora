503cf6599d0f3cf19283f752fcbb7299310fde19
vedant-shroff <vedant.shroff@somaiya.edu>
2020-08-12 00:26:26 +0530

    reformat

--- src/main/java/org/terasology/edibleFlora/system/FillingModifierSystem.java
+++ src/main/java/org/terasology/edibleFlora/system/FillingModifierSystem.java
@@ -11,15 +11,16 @@ import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.hunger.component.FoodComponent;
 import org.terasology.simpleFarming.events.ModifyFilling;
-import org.terasology.utilities.modifiable.ModifiableValue;
 
+/**
+ * This system is used to return any modified value of the FoodComponent of a seed
+ * Can be used with any modifiers for seeds
+ */
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class FillingModifierSystem extends BaseComponentSystem {
 
     @ReceiveEvent
     public void onFillingModifiedEvent(ModifyFilling event, EntityRef entity) {
-        FoodComponent foodComponent = new FoodComponent();
-        foodComponent.filling = new ModifiableValue(event.getNewFilling());
-        entity.addOrSaveComponent(foodComponent);
+        entity.getComponent(FoodComponent.class).filling.preAdd(event.getNewFilling());
     }
 }
